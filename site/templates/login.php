b0d75cd8e5696eaa032bf1c316816a4eb0a26656
Florian Posdziech <hallo@flowfx.de>
2015-03-06 20:10:23 -0600

    Add basic bootstrap styling to login form

--- site/templates/login.php
+++ site/templates/login.php
@@ -6,20 +6,19 @@
 	<div class="alert"><?php echo $page->alert()->html() ?></div>
 <?php endif ?>
 
-	<form method="post">
-		<div>
+	<form method="post" class="form-inline">
+		<div class="form-group">
 			<label for="username"><?php echo $page->username()->html() ?></label>
 			<input type="text" id="username" name="username">
 		</div>
-		<div>
+		<div class="form-group">
 			<label for="password"><?php echo $page->password()->html() ?></label>
 			<input type="password" id="password" name="password">
 		</div>
-		<div>
-			<input type="submit" name="login" value="<?php echo $page->button()->html() ?>">
+		<div class="form-group">
+			<input type="submit" class="btn btn-default" name="login" value="<?php echo $page->button()->html() ?>">
 		</div>
 	</form>
-
 
 
 
