67871cfc370a301b2d3ef4a4cd3cbb199cf318ac
Florian Posdziech <hallo@flowfx.de>
2015-03-06 20:00:52 -0600

    Add Preview to editors page
    * extra colums with preview button
    * item content (text) opens in modal view

--- site/templates/editing.php
+++ site/templates/editing.php
@@ -16,6 +16,7 @@
 		<tr>
 			<th>Slug</th>
 			<th>Titel</th>
+			<th>Vorschau</th>
 			<th>Public</th>
 			<th>Themengebiet</th>
 			<th>Klasse</th>
@@ -31,14 +32,32 @@
 		</tr>
 	</thead>
 	<tbody>
-		<?php foreach($site->children()->visible()->children() as $item): ?>
+		<?php foreach($site->children()->children() as $item): ?>
 			<tr>
 				<!-- Slug, Title, Visibility -->
 				<td><a href="<?php echo url($item) ?>"><?php echo html($item->slug()) ?></a></td>
 				<td><?php echo html($item->title()) ?></td>
+				<td><!-- Button trigger modal --><button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal-<?php echo $item->hash() ?>">Vorschau</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal-<?php echo $item->hash() ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel"><?php echo $item->title()->html() ?></h4>
+      </div>
+      <div class="modal-body">
+				<?php echo $item->text()->kirbytext() ?>
+      </div>
+    </div>
+  </div>
+</div>
+</td>
+
+				<td class="text-center"><?php e( $item->isVisible(), '<span class="glyphicon glyphicon-ok"') ?></td>
 
 				<!-- Topic, Level, Tags -->
-				<td class="text-center"><?php e( $item->isVisible(), '<span class="glyphicon glyphicon-ok"') ?></td>
 				<td><?php echo html($item->parent()->title()) ?></td>
 				<td><?php echo html($item->level()) ?></td>
 				<td><?php echo html($item->tags()) ?></td>
