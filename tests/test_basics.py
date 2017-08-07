dd190bdf66828309763fe1e4e6f289d7343b56e9
Florian Posdziech <hallo@flowfx.de>
2017-08-07 17:52:51 -0500

    Improve documentation

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -3,6 +3,18 @@
 class TestBasicViews:
 
     def test_home_page_GET(self, client):
+        # GIVEN any state
+        # WHEN calling the home page
         response = client.get('/')
 
+        # THEN it's there
         assert response.status_code == 200
+
+
+class TestExerciseModel:
+
+    def test_exercise_can_be_saved(self, db):
+        # GIVEN any state
+        # WHEN adding a new exercise to the database
+        # THEN it works
+        pass
