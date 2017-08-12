f7c8a137808b31c72a4a35433897eb1374fc6ace
Florian Posdziech <hallo@flowfx.de>
2017-08-12 16:09:41 -0500

    Remove unnecessary test

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -3,8 +3,4 @@
 
 class TestExerciseModel:
 
-    def test_exercise_can_be_saved(self, db):
-        # GIVEN any state
-        # WHEN adding a new exercise to the database
-        # THEN it works
-        pass
+    pass
