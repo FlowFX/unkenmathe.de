b8963c317b5fef7fd4a67b4af6413ebfbb10cfea
Florian Posdziech <hallo@flowfx.de>
2017-08-12 08:37:17 -0500

    Split up test code

---
+++ src/tests/test_models.py
@@ -0,0 +1,10 @@
+"""Testing the models."""
+
+
+class TestExerciseModel:
+
+    def test_exercise_can_be_saved(self, db):
+        # GIVEN any state
+        # WHEN adding a new exercise to the database
+        # THEN it works
+        pass
