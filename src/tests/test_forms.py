8709b18c912d86913db28dd587344cf5275084bf
Florian Posdziech <hallo@flowfx.de>
2017-08-12 16:06:45 -0500

    Add first forms test
    Close #29

---
+++ src/tests/test_forms.py
@@ -0,0 +1,22 @@
+"""Unit tests for the forms."""
+from um.exercises import forms
+
+import pytest
+
+
+class TestExerciseForms:
+
+    TESTPARAMS_EXERCISE = [
+        ('Dies ist eine einfach Aufgabe: $5x + 4 = 20$.', True),
+    ]
+
+    @pytest.mark.parametrize('text, validity', TESTPARAMS_EXERCISE)
+    def test_general_information_form(self, text, validity):
+        """Unit test the Exercise Form."""
+        # WHEN submitting the form with data
+        form = forms.ExerciseForm(
+            data={'text': text}
+        )
+
+        # THEN it validates -- or not
+        assert form.is_valid() is validity
