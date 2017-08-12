58c3bf7f426eb42bd7eab422dc20c94fd4f763ee
Florian Posdziech <hallo@flowfx.de>
2017-08-12 17:13:42 -0500

    Use webpack to load CSS
    Close #23

--- src/um/static/webpack.config.js
+++ src/um/static/webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
   watch: true,
 };
