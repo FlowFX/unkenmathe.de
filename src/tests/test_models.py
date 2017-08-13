cc692fe19f66019c42345d93f6c999b611dcfe50
Florian Posdziech <hallo@flowfx.de>
2017-08-13 15:23:14 -0500

    Render exercise text as HTML on model save (#40)
    * Naively implement render_html on save
    
    Close #15
    
    * Naively add render_html method
    
    * Actually render the HTML via node script
    
    * Show exercises with `text_html` in list view
    
    Close: #21
    
    * fix: Install node packages for CI build§

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -1,6 +1,38 @@
 """Testing the models."""
+from um.exercises import factories
+
+from mock import MagicMock
 
 
 class TestExerciseModel:
 
-    pass
+    def test_text_is_rendered_as_html_on_save(self, mocker):
+        # GIVEN a new Exercise with given Markdown/LaTeX text and empty text_html
+        ex = factories.ExerciseFactory.build(
+            text='## Header',
+        )
+        assert ex.text_html == ''
+
+        mocker.patch('um.exercises.models.Exercise.super_save', MagicMock(name="super_save"))
+
+        # WHEN saving the exercise
+        ex.save()
+
+        # THEN there is the rendered HTML
+        html = '''<h2>Header</h2>'''
+        assert ex.text_html.startswith(html)
+
+    def test_render_html_renders_markdown(self):
+        # GIVEN an exercise and a string of markdown text
+        md = '''# Title
+                  This is some **bold** text.
+                  '''
+        ex = factories.ExerciseFactory.build(
+            text=md,
+        )
+        # WHEN calling the render_html method with this
+        ex.render_html()
+
+        # THEN the `text_html` model field is filled with HTML
+        html = '''<h1>Title</h1>'''
+        assert ex.text_html.startswith(html)
--- src/um/exercises/models.py
+++ src/um/exercises/models.py
@@ -1,8 +1,38 @@
 """Exercise models."""
+import os
+
+# https://docs.python.org/3.6/library/shlex.html#shlex.quote
+from shlex import quote
+
 from django.db import models
+
+from Naked.toolshed.shell import muterun_js
+# >>> result = muterun_js('script.js', '"this is a string"')
+# >>> result.stdout
+# b'<p>this is a string</p>\n\n'
+# >>>	
+# scripth = './static/js/script.js'
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+script = os.path.join(MODULE_DIR, 'static/js/script.js')
 
 
 class Exercise(models.Model):
     """The main exercise model."""
 
-    text = models.TextField(verbose_name='Exercise text')
+    text = models.TextField(verbose_name='Exercise text, Markdown/LaTeX')
+    text_html = models.TextField(verbose_name='Exercise text, rendered as HTML')
+
+    def render_html(self) -> None:
+        """Render the raw Markdown/LaTeX `text` into HTML"""
+        result = muterun_js(script, quote(self.text))
+        self.text_html = result.stdout.decode()
+
+    def super_save(self, *args, **kwargs) -> None:
+        """Call the 'real' save() method."""
+        super(Exercise, self).save(*args, **kwargs)
+
+    def save(self, *args, **kwargs) -> None:
+        """Do stuff when saving the Exercise."""
+        self.render_html()
+
+        self.super_save(*args, **kwargs)             # Call the "real" save() method.
