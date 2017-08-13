30999af685e4535c242a74d04c1f717023f09500
Florian Posdziech <hallo@flowfx.de>
2017-08-13 10:01:33 -0500

    Naively implement render_html on save
    Close #15

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -1,6 +1,23 @@
 """Testing the models."""
+from um.exercises import factories
+
+from mock import MagicMock
 
 
 class TestExerciseModel:
 
-    pass
+    def test_text_is_rendered_as_html_on_save(self, mocker):
+        # GIVEN a new Exercise with given Markdown/LaTeX text and empty text_html
+        ex = factories.ExerciseFactory.build(
+            text='## Header',
+        )
+        assert ex.text_html == ''
+
+        mocker.patch('um.exercises.models.Exercise.super_save', MagicMock(name="super_save"))
+
+        # WHEN saving the exercise
+        ex.save()
+
+        # THEN there is the rendered HTML
+        html = '''<h2>Header</h2>'''
+        assert ex.text_html.startswith(html)
--- src/um/exercises/models.py
+++ src/um/exercises/models.py
@@ -5,4 +5,15 @@ from django.db import models
 class Exercise(models.Model):
     """The main exercise model."""
 
-    text = models.TextField(verbose_name='Exercise text')
+    text = models.TextField(verbose_name='Exercise text, Markdown/LaTeX')
+    text_html = models.TextField(verbose_name='Exercise text, rendered as HTML')
+
+    def super_save(self, *args, **kwargs) -> None:
+        """Call the 'real' save() method."""
+        super(Exercise, self).save(*args, **kwargs)
+
+    def save(self, *args, **kwargs) -> None:
+        """Do stuff when saving the Exercise."""
+        self.text_html = '<h2>Header</h2>'
+
+        self.super_save(*args, **kwargs)             # Call the "real" save() method.
