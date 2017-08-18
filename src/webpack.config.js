ef86ae1cec29d9299d111ee311fa7a44e6b699bf
Florian Posdziech <hallo@flowfx.de>
2017-08-18 09:31:50 -0500

    Set up django-webpack-loader
    via http://owaislone.org/blog/webpack-plus-reactjs-and-django/

---
+++ src/webpack.config.js
@@ -0,0 +1,30 @@
+let path = require('path');
+let webpack = require('webpack');
+let BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  context: __dirname,
+
+  entry: './assets/js/index.js',
+
+  output: {
+    path: path.resolve('./assets/bundles/'),
+    filename: '[name]-[hash].js',
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+};
