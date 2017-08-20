299852908aa4dba9b1966a3ec6a8f7d58f41f96a
Florian Posdziech <hallo@flowfx.de>
2017-08-20 11:03:56 -0500

    Force SSL in production

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -4,6 +4,9 @@ from .common import *
 # Core Settings
 ALLOWED_HOSTS = ['.unkenmathe.de']
 
+# Security
+SECURE_SSL_REDIRECT = True
+
 # Configure location of static files
 STATIC_ROOT = os.path.abspath('/var/www/static/unkenmathe.de')
 MEDIA_ROOT = os.path.abspath('/var/www/media/unkenmathe.de')
