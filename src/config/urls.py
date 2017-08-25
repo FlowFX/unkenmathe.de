014180cd5c3516ef71a127e221d1694554c00df3
Florian Posdziech <hallo@flowfx.de>
2017-08-25 17:29:15 -0500

    Add simple favicon

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,12 +2,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from um.core import views as core_views
+from um.exercises import views as exercises_views
 from um.exercises import urls as exercises_urls
-from um.exercises.views import ExcerciseListView
 
 
 urlpatterns = [
-    url(r'^$', ExcerciseListView.as_view(), name='index'),
+    url(r'^favicon\.ico$', core_views.favicon_view),  # redirect stupid browser looking for /favicon.ico
+    url(r'^$', exercises_views.ExcerciseListView.as_view(), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^exercises/', include(exercises_urls, namespace='exercises')),
