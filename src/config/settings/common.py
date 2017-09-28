7b3ff942e3ae7fea33f8f48cb1bea3187f55a91e
Florian Posdziech <hallo@flowfx.de>
2017-09-28 13:31:26 -0500

    Configure i18n

--- src/config/settings/common.py
+++ src/config/settings/common.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -105,6 +106,12 @@ TEMPLATES = [
         },
     },
 ]
+
+
+# Languages
+USE_I18N = True
+USE_L10N = True
+LANGUAGE_CODE = 'de-DE'
 
 
 # Email
