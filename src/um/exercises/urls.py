e5080ae87de65e3781ff7878c5cec977cf165c8f
Florian Posdziech <hallo@flowfx.de>
2017-09-28 14:16:28 -0500

    Translate URLs

--- src/um/exercises/urls.py
+++ src/um/exercises/urls.py
@@ -7,9 +7,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.ExcerciseListView.as_view(), name='index'),
     url(r'^howto$', views.HowtoView.as_view(), name='howto'),
-    url(r'^new$', views.ExerciseCreateView.as_view(), name='create'),
+    url(r'^neu$', views.ExerciseCreateView.as_view(), name='create'),
     url(r'^(?P<pk>[-\w]+)/$', views.ExerciseDetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[-\w]+)/edit$', views.ExerciseUpdateView.as_view(), name='update'),
-    url(r'^(?P<pk>[-\w]+)/delete$', views.ExerciseDeleteView.as_view(), name='delete'),
+    url(r'^(?P<pk>[-\w]+)/bearbeiten$', views.ExerciseUpdateView.as_view(), name='update'),
+    url(r'^(?P<pk>[-\w]+)/loeschen$', views.ExerciseDeleteView.as_view(), name='delete'),
     url(r'^(?P<pk>[-\w]+)/pdf$', views.exercise_pdf_view, name='pdf'),
 ]
