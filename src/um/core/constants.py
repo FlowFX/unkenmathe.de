1b2c3d4a980569bc0f9f4852b52ad70c75327fdb
Florian Posdziech <hallo@flowfx.de>
2017-10-05 11:03:54 -0500

    Add license and author attribution in PDF view
    close #271

--- src/um/core/constants.py
+++ src/um/core/constants.py
@@ -3,17 +3,27 @@ from django.utils.translation import gettext_lazy as _
 
 
 CC_BY = 'cc-by'
+CC_BY_SHORT = 'CC BY 4.0'
+CC_BY_LONG = 'Namensnennung 4.0 International (CC BY 4.0)'
+
 CC_BY_SA = 'cc-by-sa'
+CC_BY_SA_SHORT = 'CC BY-SA 4.0'
+CC_BY_SA_LONG = 'Namensnennung - Weitergabe unter gleichen Bedingungen 4.0 International (CC BY-SA 4.0)'
 
 LICENCE_CHOICES = (
-    (CC_BY, 'CC BY 4.0'),
-    (CC_BY_SA, 'CC BY-SA 4.0'),
+    (CC_BY, CC_BY_SHORT),
+    (CC_BY_SA, CC_BY_SA_SHORT),
 )
 
 LICENCE_CHOICES_LONG = (
-    (CC_BY, 'Namensnennung 4.0 International (CC BY 4.0)'),
-    (CC_BY_SA, 'Namensnennung - Weitergabe unter gleichen Bedingungen 4.0 International (CC BY-SA 4.0)'),
+    (CC_BY, CC_BY_LONG),
+    (CC_BY_SA, CC_BY_SA_LONG),
 )
+
+LICENCE_NAMES_LONG = {
+    CC_BY: CC_BY_LONG,
+    CC_BY_SA: CC_BY_LONG,
+}
 
 LICENCE_URLS = {
     CC_BY: 'https://creativecommons.org/licenses/by/4.0/deed.de',
