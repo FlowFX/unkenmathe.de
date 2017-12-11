e07ec82cdccfb34b1414df2893f0a650ffc2779c
Florian Posdziech <hallo@flowfx.de>
2017-12-11 08:45:02 -0600

    Convert url to path
    use modern, Django 2.0 URL configuration

--- src/um/exercises/urls.py
+++ src/um/exercises/urls.py
@@ -1,17 +1,16 @@
 """URL configuration for exercises app."""
 from django.urls import path
-from django.conf.urls import url
 
 from . import views
 
 
-app_name='exercises'
+app_name = 'exercises'
 urlpatterns = [
-    url(r'^$', views.ExcerciseListView.as_view(), name='index'),
-    url(r'^howto$', views.HowtoView.as_view(), name='howto'),
-    url(r'^neu$', views.ExerciseCreateView.as_view(), name='create'),
-    url(r'^(?P<slug>[-\w]+)/$', views.ExerciseDetailView.as_view(), name='detail'),
-    url(r'^(?P<slug>[-\w]+)/bearbeiten$', views.ExerciseUpdateView.as_view(), name='update'),
-    url(r'^(?P<slug>[-\w]+)/loeschen$', views.ExerciseDeleteView.as_view(), name='delete'),
-    url(r'^(?P<slug>[-\w]+)/pdf$', views.exercise_pdf_view, name='pdf'),
+    path('', views.ExcerciseListView.as_view(), name='index'),
+    path('howto', views.HowtoView.as_view(), name='howto'),
+    path('neu', views.ExerciseCreateView.as_view(), name='create'),
+    path('<str:slug>', views.ExerciseDetailView.as_view(), name='detail'),
+    path('<str:slug>/bearbeiten', views.ExerciseUpdateView.as_view(), name='update'),
+    path('<str:slug>/loeschen', views.ExerciseDeleteView.as_view(), name='delete'),
+    path('<str:slug>/pdf', views.exercise_pdf_view, name='pdf'),
 ]
--- src/um/sheets/urls.py
+++ src/um/sheets/urls.py
@@ -1,14 +1,12 @@
 """URL configuration for sheets app."""
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 
 app_name = 'sheets'
 urlpatterns = [
-    url(r'^neu$', views.SheetCreateView.as_view(), name='create'),
-    url(r'^(?P<pk>[-\w]+)/$', views.SheetDetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[-\w]+)/bearbeiten$', views.SheetUpdateView.as_view(), name='update'),
-    # url(r'^(?P<pk>[-\w]+)/delete$', views.ExerciseDeleteView.as_view(), name='delete'),
-    # url(r'^(?P<pk>[-\w]+)/pdf$', views.exercise_pdf_view, name='pdf'),
+    path('neu', views.SheetCreateView.as_view(), name='create'),
+    path('<str:pk>', views.SheetDetailView.as_view(), name='detail'),
+    path('<str:pk>/bearbeiten', views.SheetUpdateView.as_view(), name='update'),
 ]
